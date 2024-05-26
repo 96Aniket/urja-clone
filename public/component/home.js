9e3df0f3ee5151d37cd53912eb84a084c969f544
Aniket <aniketsupekar2004@gmail.com>
2024-05-26 11:57:31 +0530

    one

--- public/component/home.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-function hi(){
-    <he>anuu</he>
-}
---
+++ src/component/home.js
@@ -0,0 +1,19 @@
+
+import { Navbar } from '../view/Navbar';
+import Carousel from '../view/Carousel';
+
+
+export default function Home(){
+ return(
+ <>
+  <Navbar />
+  <Carousel />
+  </>
+  );
+}
+  
+
+  
+
+
+
