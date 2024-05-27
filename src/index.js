20b819bc8c6fc160cee887a95a928f13a0c32cb0
Aniket <aniketsupekar2004@gmail.com>
2024-05-27 15:00:56 +0530

    zal ka

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
 import Home from './component/home';
-import {Reg} from '../src/component/Career/Registration';
-import { OurNetwork } from  '../src/component/Presence/OurNetwork';
+import { Form } from '../src/view/Career';
+import { Network } from  '../src/view/Presence';
+import Card from '../src/component/Products/Products';
 
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
   const routes=createBrowserRouter([
@@ -14,12 +15,16 @@ import { createBrowserRouter,RouterProvider } from 'react-router-dom';
       
     },
     {
-      path:'/reg',
-      element:<Reg/>,
+      path:'/Career',
+      element:<Form/>,
     },
     {
-      path:'/Ournetwork',
-      element:<OurNetwork/>
+      path:'/Presence',
+      element:<Network/>
+    },
+    {
+      path:'/Card',
+      element:<Card/>
     }
   ])
 
@@ -30,7 +35,3 @@ root.render(
   <RouterProvider router={routes} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
