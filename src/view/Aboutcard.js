0f60e71f441c373417a78db066895ed8adbc5067
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-05-27 15:43:32 +0530

    done

---
+++ src/view/Aboutcard.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './Contact.css';
+
+import About from '../Components/About/About';
+// import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <About/>
+  
+);
+
+
