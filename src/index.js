5eebfe90b54318cb4897565cf8d4bbc5960b901a
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-05-27 15:15:57 +0530

    added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import Home from './component/home';
 import {Reg} from '../src/component/Career/Registration';
 import OurNetwork from '../src/component/Presence/OurNetwork';
-import Contact from '../src/component/Contact/Contact'
+
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
   const routes=createBrowserRouter([
